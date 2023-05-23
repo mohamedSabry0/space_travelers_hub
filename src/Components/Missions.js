d7e3fce441c2fdb62ddbc54e8d45f8666db438d2
kayemba <kayembejonathan5@gmail.com>
2023-05-23 16:14:46 +0200

    Add the slice

--- src/Components/Missions.js
+++ src/Components/Missions.js
@@ -1,33 +1,65 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions, joinMission, leaveMission } from '../Redux/mission/missionSlice';
 import '../Style/Mission.css';
 
 const Missions = () => {
-    const missions = useSelector((state) => state.mission);
-    const dispatch = UseDispatch();
+  const missions = useSelector((state) => state.mission.mission);
+  console.log(missions);
+  const dispatch = useDispatch();
 
-   return(
-  <table>
-    <tr>
-      <th className="#">Mission</th>
-      <th className="#">Description</th>
-      <th className="#">Status</th>
-      <th className="#">Join missions</th>
-    </tr>
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
 
-    <tr>
-      <td />
-      <td />
-      <td>
-        <div>Ative Member</div>
-        <div>None Active Member</div>
-      </td>
-      <td>
-        <button type="submit" className="Button-btn" value="#" />
-      </td>
-    </tr>
-  </table>
-);
-   }
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th className="mission">Mission</th>
+          <th className="description">Description</th>
+          <th className="status">Status</th>
+          <th className="join">Join missions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {missions.map((mission) => (
+          <tr key={mission.mission_id}>
+            <td className="mission_name_cell">{mission.mission_name}</td>
+            <td className="description_cell">{mission.description}</td>
+            <td className="status_cell">
+              {mission.reserved ? (
+                <div className="status_active_label">Active Member</div>
+              ) : (
+                <div className="status_label">Not a Member</div>
+              )}
+            </td>
+            <td className="join_button_cell">
+              {mission.reserved ? (
+                <input
+                  type="submit"
+                  className="join_mission_reserved_btn"
+                  value="Leave Mission"
+                  onClick={() => {
+                    dispatch(leaveMission(mission.mission_id));
+                  }}
+                />
+              ) : (
+                <input
+                  type="submit"
+                  className="join_mission_btn"
+                  value="Join Mission"
+                  onClick={() => {
+                    dispatch(joinMission(mission.mission_id));
+                  }}
+                />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Missions;
--- src/Redux/mission/missionSlice.js
+++ src/Redux/mission/missionSlice.js
@@ -1,23 +1,47 @@
-import { createSlice } from '@reduxjs/toolkit';
-import Missions from '../../Components/Missions';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const missionState = {
-  mission: [],
-}
+export const fetchMissions = createAsyncThunk('missions/fetchMissions', async () => {
+  // Perform the API call or data retrieval to fetch the mission data
+  // Replace the following line with your actual data fetching code
+  const response = await fetch('https://api.spacexdata.com/v3/missions');
+  const data = await response.json();
+
+  // Return the fetched data
+  return data;
+});
+
+const initialState = {
+  missions: [],
+  // Add other relevant state properties if needed
+};
 const missionSlice = createSlice({
-    name: 'mission',
-    initialState: missionState,
-    reducers: {
-        joinMission: (state, {payload}) => {
-            state.Missions = state.mission.map((mission)) =>{
-                if(mission.mission_id ! == payload) {
-                    return mission;
-                }
-                return {...mission, reserve: true};
-            }
-        }
-    }
-})
+  name: 'missions',
+  initialState,
+  reducers: {
+    joinMission(state, action) {
+      // Update the state to mark a mission as reserved
+      const missionId = action.payload;
+      const mission = state.missions.find((mission) => mission.mission_id === missionId);
+      if (mission) {
+        mission.reserved = true;
+      }
+    },
+    leaveMission(state, action) {
+      // Update the state to mark a mission as not reserved
+      const missionId = action.payload;
+      const mission = state.missions.find((mission) => mission.mission_id === missionId);
+      if (mission) {
+        mission.reserved = false;
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchMissions.fulfilled, (state, action) => {
+      // Update the state with the fetched mission data
+      state.missions = action.payload;
+    });
+  },
+});
 
-export const {joinMission} = missionSlice.actions;
-export default missionSlice.reducers;
+export const { joinMission, leaveMission } = missionSlice.actions;
+export default missionSlice.reducer;
