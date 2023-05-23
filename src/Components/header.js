73a4dc90281956089dd3e3a950a94f2df05084cc
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-05-23 11:55:53 +0300

    Add elements to the Nav

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <nav>
+    <div className="logo">
+      <img src="" alt="" />
+      <h2>Space hub</h2>
+    </div>
     <div>
-      Header
+      <NavLink>Rockets</NavLink>
+      <NavLink>Missions</NavLink>
+      <NavLink>My Profile</NavLink>
     </div>
   </nav>
 );
