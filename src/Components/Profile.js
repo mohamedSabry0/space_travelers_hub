6dae2d9b4c2771b7b466cc471abcc865d429fe0d
kayemba <kayembejonathan5@gmail.com>
2023-05-24 12:38:27 +0200

    Add profile.css

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import '../Style/Profile.css';
 
 const Profile = () => {
   const mission = useSelector((state) => state.mission);
@@ -7,15 +8,15 @@ const Profile = () => {
   const MyMissions = mission && mission.missions ? mission.missions.filter((mission) => mission.reserved === true) : [];
 
   return (
-    <section>
-      <main>
+    <section className="container">
+      <main className="header">
         <article>
           <h2>My Missions</h2>
         </article>
         {MyMissions.length === 0 ? (
           <h4>No Missions</h4>
         ) : (
-          <ul>
+          <ul className="mission-list">
             {MyMissions.map((missions) => (
               <li key={missions.mission_id}>
                 <h3>{missions.mission_name}</h3>
