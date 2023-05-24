d748e5bdc63ebbb75003e75338b5b473fef0ba1c
kayemba <kayembejonathan5@gmail.com>
2023-05-24 11:39:01 +0200

    Add display joined

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => (
-  <h1>Profile</h1>
-);
+const Profile = () => {
+  const mission = mission = useSelector((state) => state.mission.missions);
+
+  const MyMissions = mission.filter((mission) => mission.reserved === true);
+  return (
+    <section>
+      <main>
+        <article>
+          <h>My Missions</h>
+        </article>
+        {MyMissions.length === 0 ? (
+          <h4>No Mission</h4>
+        ) : (
+          <ul>
+            {MyMissions.map((missions) => (
+              <h3 key={missions.mission_id}>{missions.mission_name}</h3>
+            ))}
+          </ul>
+        )}
+      </main>
+    </section>
+  );
+};
 
 export default Profile;
