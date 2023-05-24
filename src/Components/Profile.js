5099bd594de2d0d3a75b9cd7982035bab4a9087f
kayemba <kayembejonathan5@gmail.com>
2023-05-24 12:43:07 +0200

    fix profile.js

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -5,7 +5,7 @@ import '../Style/Profile.css';
 const Profile = () => {
   const mission = useSelector((state) => state.mission);
 
-  const MyMissions = mission && mission.missions ? mission.missions.filter((mission) => mission.reserved === true) : [];
+  const Missions = mission && mission.missions ? mission.missions.filter((mission) => mission.reserved === true) : [];
 
   return (
     <section className="container">
@@ -13,11 +13,11 @@ const Profile = () => {
         <article>
           <h2>My Missions</h2>
         </article>
-        {MyMissions.length === 0 ? (
+        {Missions.length === 0 ? (
           <h4>No Missions</h4>
         ) : (
           <ul className="mission-list">
-            {MyMissions.map((missions) => (
+            {Missions.map((missions) => (
               <li key={missions.mission_id}>
                 <h3>{missions.mission_name}</h3>
               </li>
